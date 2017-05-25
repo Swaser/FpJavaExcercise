74f6fd45a62f9afdc2b6dd6cadbd58fd3d54b74f
Thomas Chassaing <Thomas.Chassaing@gmx.net>
2017-05-25 13:44:05 +0200

    Add .gitignore

--- src/main/java/ch/chassaing/fpjava/Tree.java
+++ src/main/java/ch/chassaing/fpjava/Tree.java
@@ -1,93 +1,198 @@
 package ch.chassaing.fpjava;
 
+import javaslang.control.Option;
+
 import java.util.Comparator;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "WeakerAccess"})
 public abstract class Tree<A> {
 
-    public abstract A value();
+  public abstract A value();
 
-    abstract Tree<A> left();
+  abstract Tree<A> left();
 
-    abstract Tree<A> right();
+  abstract Tree<A> right();
 
-    public abstract Tree<A> insert(A a);
+  public abstract Tree<A> insert(A a);
 
-    private static class Empty<A> extends Tree<A> {
+  public abstract boolean member(A a);
 
-        private final Comparator<A> comparator;
+  public abstract int size();
 
-        private Empty(Comparator<A> comparator) {
-            this.comparator = comparator;
-        }
+  public abstract int height();
 
-        @Override
-        public A value() {
-            throw new IllegalStateException("value() called on empty");
-        }
+  public abstract Option<A> max();
 
-        @Override
-        Tree<A> left() {
-            throw new IllegalStateException("value() called on empty");
-        }
+  public abstract Option<A> min();
 
-        @Override
-        Tree<A> right() {
-            throw new IllegalStateException("value() called on empty");
-        }
+  public abstract boolean isEmpty();
 
-        @Override
-        public Tree<A> insert(A a) {
-            return new Node<>(comparator, empty(comparator), a, empty(comparator));
-        }
+  public abstract Tree<A> remove(A a);
+
+  private static class Empty<A> extends Tree<A> {
+
+    private final Comparator<A> comparator;
+
+    private Empty(Comparator<A> comparator) {
+      this.comparator = comparator;
     }
 
-    private static class Node<A> extends Tree<A> {
-
-        private final Comparator<A> comparator;
-
-        private final Tree<A> left;
-        private final A value;
-        private final Tree<A> right;
-
-        private Node(Comparator<A> comparator, Tree<A> left, A value, Tree<A> right) {
-            this.comparator = comparator;
-            this.left = left;
-            this.value = value;
-            this.right = right;
-        }
-
-        @Override
-        public A value() {
-            return value;
-        }
-
-        @Override
-        Tree<A> left() {
-            return left;
-        }
-
-        @Override
-        Tree<A> right() {
-            return right;
-        }
-
-        @Override
-        public Tree<A> insert(A a) {
-
-            int compare = comparator.compare(a, value);
-            return compare == 0 ?
-                    new Node<>(comparator, left, a, right) :
-                    compare < 0 ?
-                            new Node<>(comparator, left.insert(a), value, right) :
-                            new Node<>(comparator, left, value, right.insert(a));
-        }
-
-
+    @Override
+    public A value() {
+      throw new IllegalStateException("value() called on empty");
     }
 
-    @SuppressWarnings("unchecked")
-    public static <A> Tree<A> empty(Comparator<A> comparator) {
-        return new Empty<>(comparator);
+    @Override
+    Tree<A> left() {
+      throw new IllegalStateException("value() called on empty");
     }
+
+    @Override
+    Tree<A> right() {
+      throw new IllegalStateException("value() called on empty");
+    }
+
+    @Override
+    public Tree<A> insert(A a) {
+      return new Node<>(comparator, empty(comparator), a, empty(comparator));
+    }
+
+    @Override
+    public boolean member(A a) {
+      return false;
+    }
+
+    @Override
+    public int size() {
+      return 0;
+    }
+
+    @Override
+    public int height() {
+      return -1;
+    }
+
+    @Override
+    public Option<A> max() {
+      return Option.none();
+    }
+
+    @Override
+    public Option<A> min() {
+      return Option.none();
+    }
+
+    @Override
+    public boolean isEmpty() {
+      return true;
+    }
+
+    @Override
+    public Tree<A> remove(A a) {
+      return this;
+    }
+  }
+
+  private static class Node<A> extends Tree<A> {
+
+    private final Comparator<A> comparator;
+
+    private final Tree<A> left;
+    private final A value;
+    private final Tree<A> right;
+    private final int size;
+    private final int height;
+
+    private Node(Comparator<A> comparator, Tree<A> left, A value, Tree<A> right) {
+      this.comparator = comparator;
+      this.left = left;
+      this.value = value;
+      this.right = right;
+      height = 1 + Math.max(left.height(), right.height());
+      size = 1 + left.size() + right.size();
+    }
+
+    @Override
+    public A value() {
+      return value;
+    }
+
+    @Override
+    Tree<A> left() {
+      return left;
+    }
+
+    @Override
+    Tree<A> right() {
+      return right;
+    }
+
+    @Override
+    public Tree<A> insert(A a) {
+
+      int compare = comparator.compare(a, value);
+      return compare < 0
+             ? new Node<>(comparator, left.insert(a), value, right)
+             : compare > 0
+               ? new Node<>(comparator, left, value, right.insert(a))
+               : new Node<>(comparator, left, a, right);
+    }
+
+    @Override
+    public boolean member(A a) {
+      int compare = comparator.compare(a, value);
+      return compare < 0
+             ? left.member(a)
+             : compare == 0 || right.member(a);
+    }
+
+    @Override
+    public int size() {
+      return size;
+    }
+
+    @Override
+    public int height() {
+      return height;
+    }
+
+    @Override
+    public Option<A> max() {
+      return right.max().orElse(() -> Option.of(value));
+    }
+
+    @Override
+    public Option<A> min() {
+      return left.min().orElse(() -> Option.of(value));
+    }
+
+    @Override
+    public boolean isEmpty() {
+      return false;
+    }
+
+    @Override
+    public Tree<A> remove(A a) {
+      int compare = comparator.compare(a, value);
+      return compare < 0
+             ? left.remove(a)
+             : compare > 0
+               ? right.remove(a)
+               : removeMerge();
+    }
+
+    private Tree<A> removeMerge() {
+
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <A> Tree<A> empty(Comparator<A> comparator) {
+    return new Empty<>(comparator);
+  }
+
+  public static <A> Tree<A> of(Comparator<A> comparator, A... as) {
+    return List.of(as)
+               .foldLeft(empty(comparator), Tree::insert);
+  }
 }
